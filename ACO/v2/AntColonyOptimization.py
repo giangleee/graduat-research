f5a5d80012a258f5205c90e5f96c4989046cf5b6
giangleee <giangle602@gmail.com>
2023-11-27 09:58:50 +0700

    Optimize code

--- ACO/v2/AntColonyOptimization.py
+++ ACO/v2/AntColonyOptimization.py
@@ -2,8 +2,12 @@
 AntColonyOptimization
 Mô hình Ant Colony Optimization giải bài toán TSP
 """
+from numpy import ndarray, zeros, ones, eye, random, array
+from typing import List, Tuple
+from functools import partial
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 
-import numpy
 
 class AntColonyOptimization:
     __number_dimension: int
@@ -12,14 +16,14 @@ class AntColonyOptimization:
     __alpha: int
     __beta: int
     __rho: float
-    __matrix: numpy.ndarray[int]
-    __pheromone_matrix: numpy.ndarray[int]
-    __prob_matrix_distance: numpy.ndarray[float]
+    __matrix: ndarray[int, int]
+    __pheromone_matrix: ndarray[int]
+    __prob_matrix_distance: ndarray[float]
     __goal_function: any
-    __generation_best_X: list
-    __generation_best_Y: list
+    __generation_best_path: list
+    __generation_best_time: list
 
-    def __init__(self, number_dimension: int, distance_matrix: numpy.ndarray[float], goal_function: any, size_population: int = 200, max_iterations: int = 50, alpha: int = 1, beta: int = 2, rho: float = 0.1):
+    def __init__(self, number_dimension: int, distance_matrix: ndarray[float], goal_function: any, size_population: int = 200, max_iterations: int = 50, alpha: int = 1, beta: int = 2, rho: float = 0.1):
         self.__number_dimension = number_dimension
         self.__size_population = size_population
         self.__max_iterations = max_iterations
@@ -31,30 +35,48 @@ class AntColonyOptimization:
         self.__rho = rho
 
         # generate matrix
-        self.__matrix = numpy.zeros(
+        self.__matrix = zeros(
             (self.__size_population, self.__number_dimension)).astype(int)  # Dòng này khởi tạo ma trận đường đi của kiến trúc Table với toàn bộ là 0 và đặt kiểu dữ liệu thành số nguyên.
-        self.__pheromone_matrix = numpy.ones(
+        self.__pheromone_matrix = ones(
             (self.__number_dimension, self.__number_dimension)).astype(int)
         # ma trận nghịch đảo của ma trận khoảng cách
         self.__prob_matrix_distance = 1 / \
-            (distance_matrix + 1e-10 * numpy.eye(number_dimension, number_dimension))
+            (distance_matrix + 1e-10 * eye(number_dimension, number_dimension))
 
         # Record the best solutions for each generation
-        self.__generation_best_X, self.__generation_best_Y = [], []
+        self.__generation_best_path, self.__generation_best_time = [], []
 
-    def update_pheromone_matrix(self, current_y: numpy.ndarray[float]):
-        delta_tau = numpy.zeros((self.__number_dimension, self.__number_dimension))
+    def update_pheromone_matrix(self, current_y: ndarray[float]):
+        delta_tau = zeros(
+            (self.__number_dimension, self.__number_dimension))
 
         for people in range(self.__size_population):
-            for dimention in range(self.__number_dimension - 1):
-                first_point, second_point = self.__matrix[people, dimention], self.__matrix[people, dimention + 1]
-                delta_tau[first_point, second_point] += 1 / current_y[people]
+            indices = self.__matrix[people]
+            delta_tau[indices[:-1], indices[1:]] += 1 / current_y[people]
+            delta_tau[indices[-1], indices[0]] += 1 / current_y[people]
 
-            first_point, second_point = self.__matrix[people, self.__number_dimension - 1], self.__matrix[people, 0]
-            delta_tau[first_point, second_point] += 1 / current_y[people]
+        self.__pheromone_matrix = (1 - self.__rho) * \
+            self.__pheromone_matrix + delta_tau
 
-        self.__pheromone_matrix = (1 - self.__rho) * self.__pheromone_matrix + delta_tau
+    def build_ant_road_matrix_base_probability(self, people: int, prob_matrix: ndarray[int, int]):
+        # Start point
+        self.__matrix[people, 0] = 0
 
+        for dimension in range(self.__number_dimension - 1):
+            # Save current visited town
+            taboo_set = set(self.__matrix[people, :dimension + 1])
+
+            # Rest of it
+            allow_visit_list = list(
+                set(range(self.__number_dimension)) - taboo_set)
+
+            prob = prob_matrix[self.__matrix[people,
+                                             dimension], allow_visit_list]
+            prob /= prob.sum()
+
+            # Choose next point based on probability
+            next_point = random.choice(allow_visit_list, p=prob)
+            self.__matrix[people, dimension + 1] = next_point
 
     def execute(self, max_iterations: int = 0):
         # handle update max iterations
@@ -66,38 +88,26 @@ class AntColonyOptimization:
                 (self.__prob_matrix_distance) ** self.__beta
 
             for people in range(self.__size_population):
-                # Start point
-                self.__matrix[people, 0] = 0
+                self.build_ant_road_matrix_base_probability(
+                    people=people, prob_matrix=prob_matrix)
+            # Xây dựng đường đi kiến trúc của kiến trúc đồng thời
+            # parallel_build = partial(
+            #     self.build_ant_road_matrix_base_probability, prob_matrix=prob_matrix)
+            # with ProcessPoolExecutor() as executor:
+            #     executor.map(parallel_build, range(self.__size_population))
 
-                for dimension in range(self.__number_dimension - 1):
-                    # save current visited town
-                    taboo_set = set(self.__matrix[people, :dimension + 1])
-                    # rest of it
-                    allow_visit_list = list(
-                        set(range(self.__number_dimension)) - taboo_set)
-
-                    prob = prob_matrix[self.__matrix[people,
-                                                     dimension], allow_visit_list]
-                    prob = prob / prob.sum()
-
-                    # choose next point base prob
-                    next_point = numpy.random.choice(
-                        allow_visit_list, size=1, p=prob)
-                    self.__matrix[people, dimension + 1] = next_point
-
-            current_y = numpy.array([self.__goal_function(element)
-                                    for element in self.__matrix])
+            current_y = array([self.__goal_function(element)
+                               for element in self.__matrix])
 
             best_index = current_y.argmin()
             best_x, best_y = self.__matrix[best_index, :].copy(
             ), current_y[best_index].copy()
-            self.__generation_best_X.append(best_x)
-            self.__generation_best_Y.append(best_y)
+            self.__generation_best_path.append(best_x)
+            self.__generation_best_time.append(best_y)
 
             self.update_pheromone_matrix(current_y=current_y)
 
-
-        best_generation = numpy.array(self.__generation_best_Y).argmin()
-        return self.__generation_best_X[best_generation], self.__generation_best_Y[best_generation]
+        best_generation = array(self.__generation_best_time).argmin()
+        return self.__generation_best_path[best_generation], self.__generation_best_time[best_generation]
 
     fit = execute
--- ACO/v2/main.py
+++ ACO/v2/main.py
@@ -1,4 +1,5 @@
 # from AntColonyOptimization import AntColonyOptimization
+import time
 import numpy
 from scipy import spatial
 from AntColonyOptimization import AntColonyOptimization
@@ -15,6 +16,7 @@ distance_matrix = spatial.distance.cdist(
 
 # optimize function
 
+
 def calculateTotalDistance(routine):
     number_dimention, = routine.shape
     return sum([distance_matrix[routine[dimention % number_dimention], routine[(
@@ -23,17 +25,49 @@ def calculateTotalDistance(routine):
 
 aco: AntColonyOptimization = AntColonyOptimization(
     number_dimension=number_dimention, distance_matrix=distance_matrix, goal_function=calculateTotalDistance)
-# print(aco.execute())
+
+
+def run_and_measure(func, num_runs=1, *args, **kwargs):
+    """
+    Run a function multiple times and measure the execution time for each run.
+
+    Parameters:
+    - func: The function to be executed.
+    - num_runs: The number of times to run the function.
+    - *args: Positional arguments to be passed to the function.
+    - **kwargs: Keyword arguments to be passed to the function.
+
+    Returns:
+    - execution_times: A list containing the execution time for each run.
+    """
+    execution_times = []
+
+    for _ in range(num_runs):
+        start_time = time.time()
+        func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        execution_times.append(execution_time)
+
+    return execution_times
+
+
+# Assuming you have an instance of AntColonyOptimization named ant_colony_optimization
+num_runs = 5  # Set the desired number of runs
+execution_times = run_and_measure(
+    aco.execute, num_runs, max_iterations=100)
+
+for i, time_taken in enumerate(execution_times, start=1):
+    print(f"Run {i}: Execution Time - {time_taken} seconds")
+
+
 best_x, best_y = aco.execute()
 
 # plot
 fig, ax = pyplot.subplots(1, 2)
 best_x_convert_to_circle = numpy.concatenate([best_x, [best_x[0]]])
 best_x_with_location = points_coordinate[best_x_convert_to_circle, :]
-print(best_x_convert_to_circle)
 ax[0].plot(best_x_with_location[:, 0], best_x_with_location[:, 1], 'o-r')
 pandas.DataFrame(
-    aco._AntColonyOptimization__generation_best_Y).cummin().plot(ax=ax[1])
+    aco._AntColonyOptimization__generation_best_time).cummin().plot(ax=ax[1])
 pyplot.show()
-
-# print(best_points_coordinate)
